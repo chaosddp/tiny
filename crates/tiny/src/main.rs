f6772ce82fec1bd501d6c03f2f35334dd52b3a3a
Chaos Yu <chaos.you@gmail.com>
2026-09-11 20:57:56 +0800

    实现 LuaHttpClient 和 LuaHttpResponse 结构，添加 HTTP 客户端功能，支持异步请求

--- crates/tiny/src/main.rs
+++ crates/tiny/src/main.rs
@@ -1,5 +1,9 @@
+mod lua_helpers;
+
 use luaenv::env::LuaEnv;
-use luaenv::lua::{Lua, LuaResult};
+use luaenv::lua::{Lua, LuaExternalResult, LuaResult};
+
+use crate::lua_helpers::http::{LuaHttpClient, LuaHttpResponse};
 
 fn t(_: &Lua, _: ()) -> LuaResult<()> {
     println!("hello, world");
@@ -7,22 +11,33 @@ fn t(_: &Lua, _: ()) -> LuaResult<()> {
     Ok(())
 }
 
+fn create_new_http_client(_: &Lua, _: ()) -> LuaResult<LuaHttpClient> {
+    let client = LuaHttpClient::new().into_lua_err()?;
+
+    Ok(client)
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let env = LuaEnv::new("tiny").unwrap();
 
-    env.add_function("test", t).unwrap();
-    env.add_function("a.b.test", t).unwrap();
-    env.add_member("a.b.pi", 3.1415).unwrap();
-    env.add_package_path("r/?.lua").unwrap();
+    //     env.add_function("test", t).unwrap();
+    //     env.add_function("a.b.test", t).unwrap();
+    //     env.add_member("a.b.pi", 3.1415).unwrap();
+    //     env.add_package_path("r/?.lua").unwrap();
 
-    env.exec_script("t.lua").await.unwrap();
+    //     env.exec_script("t.lua").await.unwrap();
 
-    env.call::<()>("print", ("hello", "-", "world"))
-        .await
+    //     env.call::<()>("print", ("hello", "-", "world"))
+    //         .await
+    //         .unwrap();
+
+    //     let ret = env.call::<i32>("sum", (1, 2)).await.unwrap();
+
+    //     println!("a + b = {}", ret);
+
+    env.add_function("http.newClient", create_new_http_client)
         .unwrap();
 
-    let ret = env.call::<i32>("sum", (1, 2)).await.unwrap();
-
-    println!("a + b = {}", ret);
+    env.exec_script("t.lua").await.unwrap();
 }
